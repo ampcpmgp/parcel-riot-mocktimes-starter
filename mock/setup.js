4b307ed99ef2535a788ea2f54a5d4692d467cbae
ampcpmgp <ampcpmgp@gmail.com>
2018-02-19 20:30:33 +0900

    wavyで無限依存で処理が終わらなかったため除去

--- mock/setup.js
+++ mock/setup.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
-import Path from '~/src/const/path'
+import Path from '../src/const/path'
 
 const mock = new MockAdapter(axios, { delayResponse: 500 })
 mock.onGet(Path.TEST).reply(200, {
