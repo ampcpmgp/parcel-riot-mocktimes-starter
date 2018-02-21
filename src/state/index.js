9f4f669e6e57c2613c2e1646ef7c373198b9045c
ampcpmgp <ampcpmgp@gmail.com>
2018-02-21 21:05:21 +0900

    Dob to on-change to nx@nx-js/observer-util (#2)
    * wip
    
    * wip
    
    * wip - nx-js/observer-utilが使えそう
    
    * wip/debounce
    
    * ちゃんと動いたところまで
    
    * 不要なmodule, 処理除去
    
    * yarn lock

--- src/state/index.js
+++ src/state/index.js
@@ -1,11 +1,17 @@
-import { observable } from 'dob'
+import { observable } from '@nx-js/observer-util'
 
 const state = observable({
   ui: {
     clickCount: 0,
+    clickHistory: [],
     addCount () {
       ++state.ui.clickCount
+      state.ui.clickHistory.push(1)
     }
+  },
+  nest: {
+    a: 1,
+    b: 2
   }
 })
 
