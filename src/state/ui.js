3f5d2c931c58763b54554972ed5b2d790d0c9cfa
ampcpmgp <ampcpmgp@gmail.com>
2018-04-01 21:05:18 +0900

    add Action

--- src/state/ui.js
+++ src/state/ui.js
@@ -1,13 +1,13 @@
-import { observable } from 'dob'
+import { observable, observe, Action } from 'dob'
 
 const ui = observable({
   clickCount: 0,
   clickHistory: []
 })
 
-export const addCount = () => {
+export const addCount = Action(() => {
   ++ui.clickCount
   ui.clickHistory.push(1)
-}
+})
 
 export default ui
