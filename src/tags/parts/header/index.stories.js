0efa4776b82839057c249c78cf83159e8f9c4205
Akira Hayatake <ampcpmgp@gmail.com>
2018-12-05 16:44:49 +0900

    add addon / background & storysource

--- src/tags/parts/header/index.stories.js
+++ src/tags/parts/header/index.stories.js
@@ -1,8 +1,15 @@
 import { mount, storiesOf } from '@storybook/riot'
 import { withKnobs, number } from '@storybook/addon-knobs'
+import { withBackgrounds } from '@storybook/addon-backgrounds'
 import './index.tag'
 
 storiesOf('Header', module)
+  .addDecorator(
+    withBackgrounds([
+      { name: 'grey', value: '#ccc', default: true },
+      { name: 'lightgreen', value: 'lightgreen' }
+    ])
+  )
   .addDecorator(withKnobs)
   .add('normal', () =>
     mount('parts-header', {
